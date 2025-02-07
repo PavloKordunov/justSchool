1d2df8079cb25a8a26d649941cefa4a4b2304c2d
PavloKordunov <102924208+PavloKordunov@users.noreply.github.com>
2025-02-07 13:25:46 +0200

    finish admin homepage

--- src/components/CountChart.tsx
+++ src/components/CountChart.tsx
@@ -23,7 +23,7 @@ const data = [
 
 const CountChart = () => {
     return ( 
-        <div className='bg-white rounded-xl w-full h-full'>
+        <div className='bg-white p-4 rounded-xl w-full h-full'>
             <div className='flex justify-between items-center'>
                 <h1 className='text-lg font-semibold'>Students</h1>
                 <Image src='/moreDark.png' alt='' width={20} height={20}/>
