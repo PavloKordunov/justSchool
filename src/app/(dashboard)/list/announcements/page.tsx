ebf336cc05e8f59c292cfc0effc4a600a890ecef
PavloKordunov <102924208+PavloKordunov@users.noreply.github.com>
2025-02-09 15:58:28 +0200

    finishing FE part

--- src/app/(dashboard)/list/announcements/page.tsx
+++ src/app/(dashboard)/list/announcements/page.tsx
@@ -1,11 +1,11 @@
+import FormModal from "@/components/FormModal";
 import Pagination from "@/components/Pagination";
 import Table from "@/components/Table";
 import TableSearch from "@/components/TableSearch";
 import { announcementsData, role } from "@/lib/data";
 import Image from "next/image";
-import Link from "next/link";
 
-type Announcment = {
+type Announcement = {
   id: number;
   title: string;
   class: string;
@@ -32,8 +32,8 @@ const columns = [
   },
 ];
 
-const AnnouncmentListPage = () => {
-  const renderRow = (item: Announcment) => (
+const AnnouncementListPage = () => {
+  const renderRow = (item: Announcement) => (
     <tr
       key={item.id}
       className="border-b border-gray-200 even:bg-slate-50 text-sm hover:bg-lamaPurpleLight"
@@ -43,18 +43,14 @@ const AnnouncmentListPage = () => {
       <td className="hidden md:table-cell">{item.date}</td>
       <td>
         <div className="flex items-center gap-2">
-            <Link href={`/list/teachers/${item.id}`}>
-                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaSky">
-                    <Image src='/edit.png' alt="" width={16} height={16} />
-                </button>
-            </Link>
-            {role === "admin" && (
-                <button className="w-7 h-7 flex items-center justify-center rounded-full bg-lamaPurple">
-                    <Image src='/delete.png' alt="" width={16} height={16} />
-                </button>
-            )}
+          {role === "admin" && (
+            <>
+              <FormModal table="announcement" type="update" data={item} />
+              <FormModal table="announcement" type="delete" id={item.id} />
+            </>
+          )}
         </div>
-    </td>
+      </td>
     </tr>
   );
 
@@ -62,7 +58,9 @@ const AnnouncmentListPage = () => {
     <div className="bg-white p-4 rounded-md flex-1 m-4 mt-0">
       {/* TOP */}
       <div className="flex items-center justify-between">
-        <h1 className="hidden md:block text-lg font-semibold">All Announcments</h1>
+        <h1 className="hidden md:block text-lg font-semibold">
+          All Announcements
+        </h1>
         <div className="flex flex-col md:flex-row items-center gap-4 w-full md:w-auto">
           <TableSearch />
           <div className="flex items-center gap-4 self-end">
@@ -72,6 +70,9 @@ const AnnouncmentListPage = () => {
             <button className="w-8 h-8 flex items-center justify-center rounded-full bg-lamaYellow">
               <Image src="/sort.png" alt="" width={14} height={14} />
             </button>
+            {role === "admin" && (
+              <FormModal table="announcement" type="create" />
+            )}
           </div>
         </div>
       </div>
@@ -83,4 +84,4 @@ const AnnouncmentListPage = () => {
   );
 };
 
-export default AnnouncmentListPage;
+export default AnnouncementListPage;
